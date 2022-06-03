8dbe3b1ef17867ba88ee214cae9ea5949ac69dbc
Tiago <tiagodev@protonmail.com>
2022-06-03 14:50:52 -0300

    feat: add globalStyles from styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import Login from "./screens/login/login";
 import theme from "./themes/main/theme";
+import { GlobalStyles } from "./themes/main/global-styles";
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <ThemeProvider theme={theme}>
-        <Login  />
+        <Login />
       </ThemeProvider>
     </>
   );
