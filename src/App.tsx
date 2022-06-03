6852817541d60be2317129fd2bf32c8b5a4f46b9
Tiago <tiagodev@protonmail.com>
2022-06-03 15:34:32 -0300

    feat: add the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { ThemeProvider } from "styled-components";
+import { Route, Routes } from "react-router-dom";
+
+import MoviesList from "./screens/movies-list/movies-list";
 import Login from "./screens/login/login";
+import { LOGIN_URL } from "./screens/login/login.types";
+import { MOVIES_LIST_URL } from "./screens/movies-list/movies-list.type";
+
 import theme from "./themes/main/theme";
 import { GlobalStyles } from "./themes/main/global-styles";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
-        <Login />
+        <Routes>
+          <Route element={<Login />} path={LOGIN_URL} />
+          <Route element={<MoviesList />} path={MOVIES_LIST_URL} />
+        </Routes>
       </ThemeProvider>
     </>
   );
