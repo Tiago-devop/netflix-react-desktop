a73d7d108ccc71b2b2f4dad0a826fedd03a77382
Tiago <tiagodev@protonmail.com>
2022-06-26 20:55:25 -0300

    feat: add saga actions

--- src/store/shows/shows.saga.ts
+++ src/store/shows/shows.saga.ts
@@ -1,27 +1,77 @@
-import showsSlice from 'store/shows/shows.slice';
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { List } from 'store/shows/shows.type';
+import { showsActions } from 'store/shows/shows.slice';
+import {
+  put,
+  call,
+  cancel,
+  select,
+  takeLatest,
+} from 'redux-saga/effects';
+import showsService from 'services/shows/shows';
 import { AxiosResponse } from 'axios';
-import { showsService } from 'services/shows/shows.type';
-
-const { setData, setError, setSettings } = showsSlice.actions;
+import { Show } from 'store/shows/shows.type';
+import { tokenSelector } from 'store/user/user.selector';
+import { Data as UserData } from 'store/user/user.type';
 
 function* getList() {
-  try {
-    yield put(setSettings({ loading: true }));
-    const response: AxiosResponse<List> = yield call(showsService().getList);
+  yield put(showsActions.setSettings({ loading: true }));
 
-    yield put(setData({ list: response.data }));
-    yield put(setError(''));
-  } catch (e) {
-    yield put(setError('Error'));
+  const token: UserData['token'] = yield select(tokenSelector);
+
+  if (!token) {
+    yield put(showsActions.setError('User token was not found'));
+    yield cancel();
+  }
+
+  try {
+    const response: AxiosResponse<Show[]> = yield call(
+      showsService({ token: token as string }).getList,
+    );
+
+    const showsList = response.data.reduce((accumulator, show) => {
+      // @ts-ignore
+      const categoryKey = accumulator[show.category] || [];
+
+      return {
+        ...accumulator,
+        [show.category]: categoryKey.concat(show),
+      };
+    }, {});
+
+    yield put(showsActions.setList(showsList));
+  } catch (exception) {
+    yield put(showsActions.setError('Error'));
   } finally {
-    yield put(setSettings({ loading: false }));
+    yield put(showsActions.setSettings({ loading: false }));
   }
 }
 
-const showSaga = [
+function* getMyList() {
+  yield put(showsActions.setSettings({ loading: true }));
+
+  const token: UserData['token'] = yield select(tokenSelector);
+
+  if (!token) {
+    yield put(showsActions.setError('User token was not found'));
+    yield cancel();
+  }
+
+  try {
+    const response: AxiosResponse<Show[]> = yield call(
+      showsService({ token: token as string }).getMyList,
+    );
+
+    yield put(showsActions.setMyList(response.data));
+    yield put(showsActions.setError(''));
+  } catch (exception) {
+    yield put(showsActions.setError('Error'));
+  } finally {
+    yield put(showsActions.setSettings({ loading: false }));
+  }
+}
+
+const showsSaga = [
   takeLatest('shows/getList', getList),
+  takeLatest('shows/getMyList', getMyList),
 ];
 
-export default showSaga;
+export default showsSaga;
